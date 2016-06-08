90c449810ca04af3c512fefc444cf9f0a76d0bdb
masterhardisk <gerardcontador9@gmail.com>
2016-06-08 17:59:56 +0200

    upload app

---
+++ app/src/main/java/com/example/gerardo/free/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gerardo.free;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar mtoolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mtoolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mtoolbar);
+    }
+
+}
