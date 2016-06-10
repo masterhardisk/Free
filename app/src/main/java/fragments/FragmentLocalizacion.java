0f400627ea29218de85a6350cc040005a6e90430
masterhardisk <gerardcontador9@gmail.com>
2016-06-10 03:52:26 +0200

    add google maps fragment

--- app/src/main/java/fragments/FragmentLocalizacion.java
+++ app/src/main/java/fragments/FragmentLocalizacion.java
@@ -1,21 +1,49 @@
 package fragments;
 
+
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.gerardo.free.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by MasterHardisk on 8/06/16.
  */
-public class FragmentLocalizacion extends Fragment{
+public class FragmentLocalizacion extends Fragment implements OnMapReadyCallback{
+    private SupportMapFragment mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_localizacion, container, false);
+       View view = inflater.inflate(R.layout.fragment_localizacion, container, false);
+        mMap = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mMap.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        map.setMyLocationEnabled(true);
+        map.setTrafficEnabled(true);
+        map.setIndoorEnabled(true);
+        map.setBuildingsEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(40.71533, 0.57694), 17));
+        map.addMarker(new MarkerOptions()
+                .title("FREESTYLE")
+                .snippet("Aqui es la fiesta")
+                .position(new LatLng(40.71533, 0.57694)));
+
     }
 }
